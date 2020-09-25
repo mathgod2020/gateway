0ebe9e30eb22efe260d6bb74c962d4d3ab999e35
mathgod2020 <62939850+mathgod2020@users.noreply.github.com>
2020-09-25 12:11:21 +0900

    Add files via upload

---
+++ exel.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+ <meta charset="UTF-8">
+ <title>Document</title>
+</head>
+<body>
+ <? echo "hdfguhbgzusgdfghdhhfgh";  ?>
+ <style>
+ </style>
+ <script>
+ </script>
+</body>
+</html>
